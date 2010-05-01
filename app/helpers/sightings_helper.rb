312abacf4084bba7f642010237962ff541866c28
Matchu <matchu1993@gmail.com>
2010-05-01 16:30:37 -0400

    no sightings message

--- app/helpers/sightings_helper.rb
+++ app/helpers/sightings_helper.rb
@@ -12,4 +12,12 @@ module SightingsHelper
     votes << [:negative, "She's gone", Vote.new(:is_positive => false)]
     votes.each { |v| v.last.sighting = sighting }
   end
+  
+  def no_sightings_message(sightings, &block)
+    options = {
+      :id => 'no-sightings'
+    }
+    options[:class] = 'hidden' unless sightings.empty?
+    content_tag :div, options, &block
+  end
 end
