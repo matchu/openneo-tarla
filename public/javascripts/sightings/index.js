8467f129e13af2731fd4808edb045b97cf001a62
Matchu <matchu1993@gmail.com>
2010-05-01 16:49:48 -0400

    basic reloadSightings function

--- public/javascripts/sightings/index.js
+++ public/javascripts/sightings/index.js
@@ -1,3 +1,7 @@
+/*
+ *   Basic layout hooks
+ */
+
 $('#new-sighting input[type=text]').placeholder().focus(function () {
   $('#before-you-submit').slideDown('fast');
 }).blur(function () {
@@ -11,3 +15,15 @@ $('#new-sighting').submit(function () {
     field.value = '';
   }
 });
+
+/*
+ *   AJAX reload sightings
+ */
+
+function reloadSightings() {
+  $.get('/sightings', function (html) {
+    var sightings = $(html).find('#sightings');
+    $('#no-sightings').toggleClass('hidden', sightings.find('li').length > 0);
+    $('#sightings').html(sightings.html());
+  });
+}
