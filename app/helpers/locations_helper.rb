6805f980db74f9fddfd794ea76d737d94499ff6d
Matchu <matchu1993@gmail.com>
2010-05-02 18:38:09 -0400

    work on styling, add moltara logo

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,9 @@
 module LocationsHelper
+  def world_logo(world)
+    if world == 'Moltara'
+      'http://images.neopets.com/items/mcf_fireeggs.gif'
+    else
+      "http://images.neopets.com/altador/altadorcup/team_logos/#{world.downcase}_50.gif"
+    end
+  end
 end
