f3f7b60e625806cc731ebfc53f903fc22eeb7103
Matchu <matchu1993@gmail.com>
2010-05-12 20:14:11 -0400

    alphabetize locations within world

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,11 +1,25 @@
+class Hash
+  def sort_keys
+    ordered_hash = ActiveSupport::OrderedHash.new
+    # natural sorting from http://www.bofh.org.uk/2007/12/16/comprehensible-sorting-in-ruby, block 1
+    keys.sort_by { |key| key.split(/(\d+)/).map { |v| v =~ /\d/ ? v.to_i : v } }.
+      each { |key| ordered_hash[key] = self[key] }
+    ordered_hash
+  end
+  
+  def sort_child_keys!
+    each { |key, value| self[key] = value.sort_keys }
+  end
+end
+
 class Location
   # Locations will be loaded in as an unordered hash; sort the keys and pass
   # the values in that order to the OrderedHash
   
-  AllByWorld = ActiveSupport::OrderedHash.new
   locations = YAML::load_file(Rails.root.join('config', 'locations.yml'))
-  locations.keys.sort.each { |key| AllByWorld[key] = locations[key] }
-  All = AllByWorld.values.map { |hash| hash.values}.flatten
+  AllByWorld = locations.sort_keys
+  AllByWorld.sort_child_keys!
+  All = AllByWorld.values.map { |hash| hash.values }.flatten
   
   def self.all_by_world
     AllByWorld
